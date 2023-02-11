bf4d0f097904cffdf9cd037b80d2d32c41e4cddd
Futaba3 <nyokinyoki41aya@gmail.com>
2023-02-11 10:33:38 +0900

    add bgSwitcher

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function ($) {
+    $(".firstView").bgswitcher({
+        images: ["images/america1.NEF.jpg", "images/america2.NEF.jpg", "images/america3.NEF.jpg"],
+        interval: 5000,
+        loop: true
+    });
+});
