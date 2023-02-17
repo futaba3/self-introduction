b34d44cec1f2a5b6069cf6e1599021bfb6233ce7
Futaba3 <nyokinyoki41aya@gmail.com>
2023-02-18 02:33:00 +0900

    favicon, description, OGP

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function ($) {
     $(".firstView").bgswitcher({
-        images: ["images/america1.NEF.jpg", "images/america2.NEF.jpg", "images/america3.NEF.jpg"],
+        images: ["images/america1.NEF.jpg", "images/america2.NEF.jpg", "images/america3.NEF.jpg", "images/enkakuji.NEF.jpg", "images/stadium1.JPG", "images/stadium2.JPG", "images/stadium3.JPG", "images/yososukaStadium.jpg"],
         interval: 5000,
         loop: true
     });
